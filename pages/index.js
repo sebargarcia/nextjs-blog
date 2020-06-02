50f280cedac37afb5a1fa95a96c06ad205521b14
Sebastian Garcia <sebargarcia@gmail.com>
2020-06-02 19:16:43 -0300

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,52 +1,39 @@
 import Head from 'next/head'
+import Link from 'next/link'
 
-export default function Home() {
+import Date from '../components/date'
+import Layout from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
+
+
+export default function Home({ allPostsData }) {
   return (
-    <div className="container">
+    <Layout home={true}>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog 🍺</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href="/posts/[id]" as={`/posts/${id}`}>
+                {title}
+              </Link>
+              <br />
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+              <small className={utilStyles.lightText}>
+                {date}
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/zeit/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
 
       <footer>
         <a
@@ -54,13 +41,12 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
+          2020 Todos los derechos reservados
         </a>
       </footer>
 
       <style jsx>{`
-        .container {
+        .container {          
           min-height: 100vh;
           padding: 0 0.5rem;
           display: flex;
@@ -204,6 +190,16 @@ export default function Home() {
           box-sizing: border-box;
         }
       `}</style>
-    </div>
+    </Layout>
   )
 }
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
