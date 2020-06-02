4c9368a38cc97a891f383260265a767cc9d6c024
Sebastian Garcia <sebargarcia@gmail.com>
2020-06-02 19:31:34 -0300

    minot fixes and added missing dependencies

--- components/layout.js
+++ components/layout.js
@@ -3,8 +3,8 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = 'Sebastian'
-export const siteTitle = 'Next.js Sample Website'
+const name = 'Sebastián García'
+export const siteTitle = 'Sebastian Blog'
 
 export default function Layout({ children, home }) {
     return (
--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home({ allPostsData }) {
   return (
     <Layout home={true}>
       <Head>
-        <title>Create Next App</title>
+        <title>Sebastian Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
